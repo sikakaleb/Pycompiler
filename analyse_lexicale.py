9834d755a86016a667d23d96ad023e476b78a419
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-04-16 18:15:36 +0200

    modification de l'analyseur lexicale pour la reconnaissance des tokens maquant aprés refactor #1

--- analyse_lexicale.py
+++ analyse_lexicale.py
@@ -1,33 +1,44 @@
 import sys
 from sly import Lexer
 
+import sys
+from sly import Lexer
+
 
 class FloLexer(Lexer):
-    # Noms des lexèmes (sauf les litéraux). En majuscule. Ordre non important
     tokens = {
         IDENTIFIANT,
         ENTIER,
         ENT,
         ECRIRE,
         INFERIEUR_OU_EGAL,
+        TANTQUE,
+        BOOLEEN,
         SI,
         ALORS,
-        TANTQUE,
         SINON,
-        BOOLEEN,
-        AFFICHER
+        ET,
+        OU,
+        NON,
+        PLUS,
+        MINUS,
+        MULT,
+        DIV
     }
 
-    # Les caractères litéraux sont des caractères uniques qui sont retournés tel quel quand rencontré par l'analyse lexicale.
-    # Les litéraux sont vérifiés en dernier, après toutes les autres règles définies par des expressions régulières.
-    # Donc, si une règle commence par un de ces littérals (comme INFERIEUR_OU_EGAL), cette règle aura la priorité.
     literals = {'+', '*', '-', '/', '%', '!',
-                '{', '}', '(', ')', ',', ";", "=", '<', '>', 'et', 'ou', 'non'}
+                '[', ']', '{', '}', '(', ')', ',', ";", "=", '<', '>', '&'}
 
-    # Chaînes contenant les caractères à ignorer. Ici espace et tabulation
+    ET = r'et'
+    OU = r'ou'
+    NON = r'non'
+    PLUS = r'\+'
+    MINUS = r'-'
+    MULT = r'\*'
+    DIV = r'/'
+
     ignore = ' \t'
 
-    # Expressions régulières correspondant aux différents Lexèmes par ordre de priorité
     INFERIEUR_OU_EGAL = r'<='
 
     @_(r'0|[1-9][0-9]*')
@@ -35,11 +46,8 @@ class FloLexer(Lexer):
         t.value = int(t.value)
         return t
 
-    # Cas général
-    # En général, variable ou nom de fonction
     IDENTIFIANT = r'[a-zA-Z][a-zA-Z0-9_]*'
 
-    # Cas spéciaux:
     IDENTIFIANT['entier'] = ENT
     IDENTIFIANT['booleen'] = BOOLEEN
     IDENTIFIANT['ecrire'] = ECRIRE
@@ -48,15 +56,12 @@ class FloLexer(Lexer):
     IDENTIFIANT['sinon'] = SINON
     IDENTIFIANT['tantque'] = TANTQUE
 
-    # Syntaxe des commentaires à ignorer
     ignore_comment = r'\#.*'
 
-    # Permet de conserver les numéros de ligne. Utile pour les messages d'erreurs
     @_(r'\n+')
     def ignore_newline(self, t):
         self.lineno += t.value.count('\n')
 
-    # En cas d'erreur, indique où elle se trouve
     def error(self, t):
         print(f'Ligne {self.lineno}: caractère inattendu "{t.value[0]}"')
         self.index += 1
